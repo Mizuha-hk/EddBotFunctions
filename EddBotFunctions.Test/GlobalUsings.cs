d75d1b139fcc68167e87cd42a696dba629ce3466
Mizuha-hk <108075796+Mizuha-hk@users.noreply.github.com>
2023-09-09 13:02:31 +0900

    [add]: package & models

---
+++ EddBotFunctions.Test/GlobalUsings.cs
@@ -0,0 +1,4 @@
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using EddBotFunctions;
+global using EddBotFunctions.Shared;
+global using EddBotFunctions.Shared.Models;
---
+++ EddBotFunctions/Shared/Models/MkEventReqModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EddBotFunctions.Shared.Models
+{
+    public class MkEventReqModel
+    {
+        public string? Category { get; set; }
+    }
+}
